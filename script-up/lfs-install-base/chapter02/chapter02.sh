e260399213ac0e7000a19459deb17cc68391386f
dev.dertin <dev.dertin@48c71f52-d934-11dd-a8b6-f5aca7b582bb>
2009-01-13 16:10:39 +0000

    Add: 2.3. Start a Swap Partition

--- script-up/lfs-install-base/chapter02/chapter02.sh
+++ script-up/lfs-install-base/chapter02/chapter02.sh
@@ -9,6 +9,8 @@
 # This is free software, and you are welcome to redistribute it
 # under certain conditions; see COPYING for details.
 
+# Amended by: Guillermo Céspedes Tabárez
+# 13/01/2009
 
 function Chapter02 ()
 {
@@ -17,10 +19,13 @@ function Chapter02 ()
 	# 2.2. Creating a New Partition
 	source "$LFS_INSTALL"/chapter02/creatingpartition.sh
 
-	# 2.3. Creating a File System on the Partition
+	# 2.3. Start a Swap Partition
+	source "$LFS_INSTALL"/chapter02/startswap.sh
+	
+	# 2.4. Creating a File System on the Partition
 	source "$LFS_INSTALL"/chapter02/creatingfilesystem.sh
 
-	# 2.4. Mounting the New Partition
+	# 2.5. Mounting the New Partition
 	"$LFS_INSTALL"/chapter02/mounting.sh
 }
 
