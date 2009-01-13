8e088f97b25178365bddea90a32047f79ab4e193
dev.dertin <dev.dertin@48c71f52-d934-11dd-a8b6-f5aca7b582bb>
2009-01-13 17:42:32 +0000

    Cambio del Servidor de Package.

--- script-up/lfs-install-base/chapter03/introduction.sh
+++ script-up/lfs-install-base/chapter03/introduction.sh
@@ -13,30 +13,32 @@
 function Introduction ()
 {
 	# 3.1. Introduction
+	
 	echo
 	echo -n "Enter server [ftp://ftp.linuxfromscratch.org/pub/lfs/]: "
 	read SERVER
-
+	
 	if [ -z "$SERVER" ]
 	then
-		SERVER="ftp://ftp.linuxfromscratch.org/pub/lfs/"
+		SERVER="http://kerrek.linuxfromscratch.org/pub/lfs/"
 	fi
-
+	
 	cd "$LFS"
-	wget --quiet --delete-after "$SERVER"/lfs-packages/conglomeration/MD5SUMS
-
+	wget --quiet --delete-after "$SERVER"/conglomeration/MD5SUMS
+	
 	if [ "$?" != "0" ]
 	then
 		echo "ESERVER: '"$SERVER"' is not a valid lfs mirror."
-
+	
 		Introduction
 	fi
-
+	
 	export SERVER
-
+	
+	
 	# Begin commands
 	mkdir $LFS/sources
-
+	
 	chmod a+wt $LFS/sources
 	# End commands
 }
