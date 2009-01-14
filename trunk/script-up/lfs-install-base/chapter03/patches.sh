f066c959ddd48b845c8ed6f065d07dd26de3678b
dev.dertin <dev.dertin@48c71f52-d934-11dd-a8b6-f5aca7b582bb>
2009-01-14 01:50:45 +0000

    Update URL.

--- trunk/script-up/lfs-install-base/chapter03/patches.sh
+++ trunk/script-up/lfs-install-base/chapter03/patches.sh
@@ -15,27 +15,34 @@ function Patches ()
 	# 3.2. Needed Patches
 	cd "$LFS"/sources
 
-	wget "$SERVER"/lfs-packages/conglomeration/bash/bash-3.0-display_wrap-1.patch
-	wget "$SERVER"/lfs-packages/conglomeration/coreutils/coreutils-5.2.1-suppress_uptime_kill_su-1.patch
-	wget "$SERVER"/lfs-packages/conglomeration/coreutils/coreutils-5.2.1-uname-2.patch
-	wget "$SERVER"/lfs-packages/conglomeration/expect/expect-5.42.1-spawn-1.patch
-	wget "$SERVER"/lfs-packages/conglomeration/flex/flex-2.5.31-debian_fixes-2.patch
-	wget "$SERVER"/lfs-packages/conglomeration/gcc/gcc-3.4.1-linkonce-1.patch
-	wget "$SERVER"/lfs-packages/conglomeration/gcc/gcc-3.4.1-no_fixincludes-1.patch
-	wget "$SERVER"/lfs-packages/conglomeration/gcc/gcc-3.4.1-specs-1.patch
-	wget "$SERVER"/lfs-packages/conglomeration/inetutils/inetutils-1.4.2-kernel_headers-1.patch
-	wget "$SERVER"/lfs-packages/conglomeration/inetutils/inetutils-1.4.2-no_server_man_pages-1.patch
-	wget "$SERVER"/lfs-packages/conglomeration/iproute2/iproute2-2.6.8_040823-remove_db-1.patch
-	wget "$SERVER"/lfs-packages/conglomeration/man/man-1.5o-80cols-1.patch
-	wget "$SERVER"/lfs-packages/conglomeration/mktemp/mktemp-1.5-add_tempfile-1.patch
-	wget "$SERVER"/lfs-packages/conglomeration/perl/perl-5.8.5-libc-1.patch
-	wget "$SERVER"/lfs-packages/conglomeration/readline/readline-5.0-display_wrap-1.patch
-	wget "$SERVER"/lfs-packages/conglomeration/sysklogd/sysklogd-1.4.1-kernel_headers-1.patch
-	wget "$SERVER"/lfs-packages/conglomeration/sysklogd/sysklogd-1.4.1-signal-1.patch
-	wget "$SERVER"/lfs-packages/conglomeration/sysvinit/sysvinit-2.85-proclen-1.patch
-	wget "$SERVER"/lfs-packages/conglomeration/texinfo/texinfo-4.7-segfault-1.patch
-	wget "$SERVER"/lfs-packages/conglomeration/util-linux/util-linux-2.12b-sfdisk-2.patch
-	wget "$SERVER"/lfs-packages/conglomeration/zlib/zlib-1.2.1-security-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/automake-1.10.1-test_fix-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/bash-3.2-fixes-8.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/db-4.7.25-upstream_fixes-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/binutils-2.18-GCC43-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/binutils-2.18-configure-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/bzip2-1.0.5-install_docs-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/coreutils-6.12-i18n-2.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/coreutils-6.12-old_build_kernel-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/coreutils-6.12-uname-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/diffutils-2.8.1-i18n-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/expect-5.43.0-spawn-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/expect-5.43.0-tcl_8.5.5_fix-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/glibc-2.8-20080929-iconv_tests-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/glibc-2.8-20080929-ildoubl_test-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/grep-2.5.3-debian_fixes-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/grep-2.5.3-upstream_fixes-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/groff-1.18.1.4-debian_fixes-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/grub-0.97-disk_geometry-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/grub-0.97-256byte_inode-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/inetutils-1.5-no_server_man_pages-2.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/kbd-1.14.1-backspace-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/module-init-tools-3.4.1-manpages-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/ncurses-5.6-coverity_fixes-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/perl-5.10.0-consolidated-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/procps-3.2.7-watch_unicode-1.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/readline-5.2-fixes-5.patch
+    wget http://www.linuxfromscratch.org/patches/lfs/6.4/vim-7.2-fixes-3.patch
+
 
 	cd -
 }
