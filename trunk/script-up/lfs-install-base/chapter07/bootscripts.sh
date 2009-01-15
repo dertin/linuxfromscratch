9e03fc25b93dc46c82cc2f581a568faa2b4ec3cf
dev.dertin <dev.dertin@48c71f52-d934-11dd-a8b6-f5aca7b582bb>
2009-01-15 21:09:36 +0000

    Update String.

--- trunk/script-up/lfs-install-base/chapter07/bootscripts.sh
+++ trunk/script-up/lfs-install-base/chapter07/bootscripts.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# LFS-Install - An Automated Linux From Scratch-Installer
+# LFS-Automatic - An Automated Linux From Scratch-Installer
 # Copyright (C) 2009  Team Developer Dertin GNU/Linux
 #
-# This project is based on "http://savannah.nongnu.org/projects/lfs-install/"
+# This project is based on "http://savannah.nongnu.org/projects/LFS-Automatic/"
 #
-# LFS-Install comes with ABSOLUTELY NO WARRANTY; for details see COPYING.
+# LFS-Automatic comes with ABSOLUTELY NO WARRANTY; for details see COPYING.
 # This is free software, and you are welcome to redistribute it
 # under certain conditions; see COPYING for details.ls.
 
