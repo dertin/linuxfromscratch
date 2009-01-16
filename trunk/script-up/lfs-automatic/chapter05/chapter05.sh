a79bedbf19343f31aceaf9cab925fbe8785e717c
dev.dertin <dev.dertin@48c71f52-d934-11dd-a8b6-f5aca7b582bb>
2009-01-16 16:59:14 +0000

    Update String.

--- trunk/script-up/lfs-automatic/chapter05/chapter05.sh
+++ trunk/script-up/lfs-automatic/chapter05/chapter05.sh
@@ -38,7 +38,7 @@ function Chapter05 ()
 	# 5.9. Adjusting the Toolchain
 	Settingenvironment "$LFS_INSTALL"/chapter05/adjusting.sh						#OK
 
-	# 5.10. Tcl-8.4.7
+	# 5.10. tcl8.5.5
 	Settingenvironment "$LFS_INSTALL"/chapter05/tcl.sh
 
 	# 5.11. Expect-5.42.1
