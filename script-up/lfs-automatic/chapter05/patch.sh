c7d852d937a3d920fb253027143d784289fb8af4
dev.dertin <dev.dertin@48c71f52-d934-11dd-a8b6-f5aca7b582bb>
2009-01-17 20:15:14 +0000

    Update & Fix.

--- script-up/lfs-automatic/chapter05/patch.sh
+++ script-up/lfs-automatic/chapter05/patch.sh
@@ -3,7 +3,7 @@
 # LFS-Automatic - An Automated Linux From Scratch-Installer
 # Copyright (C) 2009  Team Developer Dertin GNU/Linux
 #
-# 
+#
 #
 # LFS-Automatic comes with ABSOLUTELY NO WARRANTY; for details see COPYING.
 # This is free software, and you are welcome to redistribute it
@@ -14,17 +14,19 @@ function Patch ()
 {
 	# 5.26. Patch-2.5.4
 	cd "$LFS"/sources
-	tar xfj patch-2.5.4.tar.bz2
+	tar xzf patch-2.5.4.tar.gz
 	cd patch-2.5.4
 
 	# 5.26.1. Installation of Patch
 
 	# Begin commands
-	CPPFLAGS=-D_GNU_SOURCE ./configure --prefix=/tools
-
+	
+	./configure --prefix=/tools
+	
 	make
 
 	make install
+	
 	# End commands
 
 	cd ..
