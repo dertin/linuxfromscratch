adcb25c0901147f14469603e3240d5df4a4b9c53
dev.dertin <dev.dertin@48c71f52-d934-11dd-a8b6-f5aca7b582bb>
2009-01-26 21:39:27 +0000

    Format Code.

--- script-up/lfs-automatic/partition/main.sh
+++ script-up/lfs-automatic/partition/main.sh
@@ -3,7 +3,6 @@
 # LFS-Automatic - An Automated Linux From Scratch-Installer
 # Copyright (C) 2009  Team Developer Dertin GNU/Linux
 #
-#
 # LFS-Automatic comes with ABSOLUTELY NO WARRANTY; for details see COPYING.
 # This is free software, and you are welcome to redistribute it
 # under certain conditions; see COPYING for details.
@@ -11,21 +10,21 @@
 # Amended by: Guillermo Céspedes Tabárez
 # 13/01/2009
 
-function Chapter02 ()
+function Partition ()
 {
-	# Chapter 2. Preparing a New Partition
+	# Preparing a New Partition
 
 	# 2.2. Creating a New Partition
-	source "$LFS_INSTALL"/chapter02/creatingpartition.sh
+	"$LFS_INSTALL"/partition/creatingpartition.sh
 
 	# 2.3. Start a Swap Partition
-	source "$LFS_INSTALL"/chapter02/startswap.sh
+	"$LFS_INSTALL"/partition/startswap.sh
 	
 	# 2.4. Creating a File System on the Partition
-	source "$LFS_INSTALL"/chapter02/creatingfilesystem.sh
+	"$LFS_INSTALL"/partition/creatingfilesystem.sh
 
 	# 2.5. Mounting the New Partition
-	"$LFS_INSTALL"/chapter02/mounting.sh
+	"$LFS_INSTALL"/partition/mounting.sh
 }
 
-Chapter02
+Partition
