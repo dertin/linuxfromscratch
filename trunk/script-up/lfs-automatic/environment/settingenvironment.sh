e219ae54f048fbfdb2a6694bc1155caa41460079
dev.dertin <dev.dertin@48c71f52-d934-11dd-a8b6-f5aca7b582bb>
2009-01-26 21:54:52 +0000

    Format Code. Add -u, --user

--- trunk/script-up/lfs-automatic/environment/settingenvironment.sh
+++ trunk/script-up/lfs-automatic/environment/settingenvironment.sh
@@ -11,28 +11,22 @@
 
 function Settingenvironment ()
 {
-	# 4.4. Setting Up the Environment
+	# Setting Up the Environment
 
 	# Begin commands
-	#cat > ~/.bash_profile << "EOF"
-	#exec env -i HOME="$HOME" TERM="$TERM" PS1="\u:\w\$ " /bin/bash
-	#EOF
+	cat > ~/.bash_profile << "EOF"
+	exec env -i HOME="$HOME" TERM="$TERM" PS1="\u:\w\$ " /bin/bash
+	EOF
 
-	#cat > ~/.bashrc << "EOF"
-	#set +h
-	#umask 022
-	#LFS="/mnt/lfs"
-	#LC_ALL="POSIX"
-	#PATH="/tools/bin:/bin:/usr/bin"
-	#export LFS LC_ALL PATH
-	#EOF
+	cat > ~/.bashrc << "EOF"
+	set +h
+	umask 022
+	LFS="/mnt/lfs"
+	LC_ALL="POSIX"
+	PATH="/tools/bin:/bin:/usr/bin"
+	export LFS LC_ALL PATH
+	EOF
 
-	#source ~/.bash_profile
+	source ~/.bash_profile
 	# End commands
-
-	sudo -u lfs \
-		env -i HOME="$HOME" TERM="$TERM" PS1="\u:\w\$ " \
-		LFS="$LFS" LC_ALL="POSIX" PATH="/tools/bin:/bin:/usr/bin" \
-		TEST_SUITES="$TEST_SUITES" \
-		/bin/bash +h $1
 }
