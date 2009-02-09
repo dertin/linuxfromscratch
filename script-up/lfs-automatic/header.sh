f993ca639e2eb2bf19aa695c04ae007d83576ea5
dev.dertin <dev.dertin@48c71f52-d934-11dd-a8b6-f5aca7b582bb>
2009-02-09 17:42:56 +0000

    Add file All Header include.

---
+++ script-up/lfs-automatic/header.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# LFS-Automatic - An Automated Linux From Scratch-Installer
+# Copyright (C) 2009  Team Developer Dertin GNU/Linux
+#
+# LFS-Automatic comes with ABSOLUTELY NO WARRANTY; for details see COPYING.
+# This is free software, and you are welcome to redistribute it
+# under certain conditions; see COPYING for details.
+
+#Header:
+	source "$LFS_INSTALL"/config/lfs-automatic.conf
+	source "$LFS_INSTALL"/function/check-function.func
+	source "$LFS_INSTALL"/function/general-function.func
