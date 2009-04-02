4eef7afc218980591ef58109ef5c06b13942cf11
dev.dertin <dev.dertin@48c71f52-d934-11dd-a8b6-f5aca7b582bb>
2009-04-02 12:25:21 +0000

    Add app-setup

---
+++ trunk/script-up/lfs-automatic/app-setup/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# LFS-Automatic - An Automated Linux From Scratch-Installer
+# Copyright (C) 2009  Team Developer Dertin GNU/Linux
+#
+#
+# LFS-Automatic comes with ABSOLUTELY NO WARRANTY; for details see COPYING.
+# This is free software, and you are welcome to redistribute it
+# under certain conditions; see COPYING for details.
+
+#RUN ROOT
+function Install-app ()
+{
+	#Copy the entire application to "$LFS_SETUP"
+	cp "$LFS_TEMP" "$LFS_SETUP"
+	
+	#Give all the permissions (test mode)
+	cd "$LFS_SETUP"
+	if [ "$(id -u)" = "0" ]
+	then
+	chmod -R 777 *
+	fi
+}
+Install-app
