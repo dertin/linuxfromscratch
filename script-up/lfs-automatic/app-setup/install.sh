a741d745b6b29968e78d2a133de8c7cf53926757
dev.dertin <dev.dertin@48c71f52-d934-11dd-a8b6-f5aca7b582bb>
2009-04-10 23:35:57 +0000

    no significant changes!

--- script-up/lfs-automatic/app-setup/install.sh
+++ script-up/lfs-automatic/app-setup/install.sh
@@ -8,17 +8,22 @@
 # This is free software, and you are welcome to redistribute it
 # under certain conditions; see COPYING for details.
 
+# By: Guillermo Céspedes Tabárez
+# Created: 02/04/2009
+# Last modified: 19/04/2009
+
 #RUN ROOT 
 function Install-app ()
 {
 	#Copy the entire application to "$LFS_SETUP"
 	cp -R "$LFS_TEMP" "$LFS_SETUP"
 	
-	#Give all the permissions (test mode)
+	#Give all the permissions
 	cd "$LFS_SETUP"
 	if [ "$(id -u)" = "0" ]
 	then
 	chmod -R +x * 
 	fi
 }
+
 Install-app
