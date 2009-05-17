f6283b6d24776b44cddc8bbd05fce81a94285270
dev.dertin <dev.dertin@48c71f52-d934-11dd-a8b6-f5aca7b582bb>
2009-05-17 00:31:39 +0000

    Add Batch [.svn folders clean]

---
+++ script-up/lfs-automatic/clearsvn.bat
@@ -0,0 +1,27 @@
+rmdir .svn /s
+
+cd app-setup/
+rmdir .svn /s
+
+cd ../function/
+rmdir .svn /s
+
+cd ../config/
+rmdir .svn /s
+
+cd ../partition/
+rmdir .svn /s
+
+cd ../scripts/
+rmdir .svn /s
+
+cd ../sources/
+rmdir .svn /s
+
+cd ../temp-system/
+rmdir .svn /s
+
+cd ../user/
+rmdir .svn /s
+
+@pause
